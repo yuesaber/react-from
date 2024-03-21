b74eb8bb8d33ed8e40f80e15f6e53c73b5053214
yuesaber <yoyuehappy@gmail.com>
2024-03-21 12:08:54 +0545

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import fbImage from './assets/fb.png';
+import xImage from './assets/x.png';
+import ggImage from './assets/gg.png';
 
 function App() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleToggle = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero">
+      <div className="form-box">
+        <div className="button-box">
+          <div id="btn" className={isLogin ? '' : 'move'}></div>
+          <button
+            type="button"
+            className={`toggle-btn ${isLogin ? 'active' : ''}`}
+            onClick={handleToggle}
+          >
+            Login
+          </button>
+          <button
+            type="button"
+            className={`toggle-btn ${isLogin ? '' : 'active'}`}
+            onClick={handleToggle}
+          >
+            Register
+          </button>
+        </div>
+        <div className="social-icons">
+          <img src={fbImage} alt="Facebook" />
+          <img src={xImage} alt="X" />
+          <img src={ggImage} alt="Google" />
+        </div>
+        <form className="input-group" id={isLogin ? 'login' : 'register'}>
+          <input type="text" className="input-field" placeholder={isLogin ? 'username or email' : 'username'} required />
+          {!isLogin && <input type="email" className="input-field" placeholder="email" required />}
+          <input type="password" className="input-field" placeholder="password" required />
+          {!isLogin && (
+            <React.Fragment>
+              <input type="checkbox" className="check-box" /><span>I agree terms & conditions</span>
+            </React.Fragment>
+          )}
+          <button type="submit" className="submit-btn">{isLogin ? 'Login' : 'Register'}</button>
+        </form>
+      </div>
     </div>
   );
 }
